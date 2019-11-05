d5fed2a3f3ec3e5e71b6b2b49a9f643481b27349
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-11-05 23:02:26 +0000

    fix: attempt to use a PR of arcalos

--- ci.sh
+++ ci.sh
@@ -5,9 +5,17 @@ set -u
 set -o pipefail
 
 NAME=$1
+ARCALOS_SHA="ffedbed6f111f72b6f1846255cbf054467a14038"
 
 git clone https://github.com/cloudbees/arcalos
+
 pushd arcalos
+  if [ "" != "${ARCALOS_SHA}" ]
+  then 
+    BRANCH_NAME="pr-${ARCALOS_SHA}"
+    git fetch origin ${ARCALOS_SHA} && git branch ${BRANCH_NAME} ${ARCALOS_SHA} && git checkout ${BRANCH_NAME}
+  fi
+
   sed -i "s|BOOT_GIT_REF=.*$|BOOT_GIT_REF=$PULL_PULL_SHA|g" ./templates/.secrets.defaults
   cat ./templates/.secrets.defaults
   ./create_aps_consumer_project.sh $NAME
