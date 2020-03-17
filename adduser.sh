954c23019c6679ddff6940376fed70af60dd5868
Cai Cooper <caicooper82@gmail.com>
2020-03-17 09:17:17 +0000

    fix: hopefully the environment variable is available within this template

--- adduser.sh
+++ adduser.sh
@@ -9,23 +9,4 @@ else
     jx create user --login=${USER_EMAIL}
   fi
   jx edit userroles --login=${USER_EMAIL} --role=owner
-cat <<EOF | kubectl apply -f -
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  labels:
-    jenkins.io/created-by: jx
-    team: jx
-  name: jx-env-view
-  namespace: jx
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: jx-env-view
-subjects:
-- apiGroup: rbac.authorization.k8s.io
-  kind: User
-  name: "${USER_EMAIL}"
-  namespace: jx
-EOF
 fi
